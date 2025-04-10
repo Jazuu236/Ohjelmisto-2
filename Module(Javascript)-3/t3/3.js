2bf096dc08461adc032be93d180cbd823939eee8
Jazuu236 <jasper.t.jarvinen@gmail.com>
2025-04-10 14:51:26 +0300

    Module-3

---
+++ Module(Javascript)-3/t3/3.js
@@ -0,0 +1,11 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+const targetElement = document.getElementById("target");
+
+let html = "";
+
+for (let i = 0; i < names.length; i++) {
+    html += `<li>${names[i]}</li>`;
+}
+
+targetElement.innerHTML = html;
